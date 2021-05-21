e5d6d9eefc5bc85fd1b4af2aeac9e2551a50d703
Tanesal <dguchinsky@yahoo.com>
2021-05-22 00:43:29 +0600

    fix: 'body-parser' is deprecated

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ var db = require("./db");
 var user = require("./controllers/usercontroller");
 var game = require("./controllers/gamecontroller");
 
+require("dotenv").config();
+
 db.sequelize.sync();
-app.use(require("body-parser"));
+
+app.use(express.json());
+
 app.use("/api/auth", user);
 app.use(require("./middleware/validate-session"));
 app.use("/api/game", game);
-app.listen(function () {
-  console.log("App is listening on 4000");
+
+app.listen(process.env.LISTEN_PORT, function () {
+  console.log(
+    `App is listening on http://localhost:${process.env.LISTEN_PORT}`
+  );
 });
