e389da6636a2dcbf57a2c5d6ba94f3c972cf70fa
Tanesal <dguchinsky@yahoo.com>
2021-05-21 22:37:50 +0600

    fix: TypeError: db.sync is not a function

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var db = require('./db');
-var user = require('./controllers/usercontroller');
-var game = require('./controllers/gamecontroller')
+var db = require("./db");
+var user = require("./controllers/usercontroller");
+var game = require("./controllers/gamecontroller");
 
-
-db.sync();
-app.use(require('body-parser'));
-app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'))
-app.use('/api/game', game);
-app.listen(function() {
-    console.log("App is listening on 4000");
-})
+db.sequelize.sync();
+app.use(require("body-parser"));
+app.use("/api/auth", user);
+app.use(require("./middleware/validate-session"));
+app.use("/api/game", game);
+app.listen(function () {
+  console.log("App is listening on 4000");
+});
